91c0205e905d096e233e8872e593e26e87e2b468
masiini <maksim.niskanen@live.fi>
2025-10-07 17:18:02 +0300

    ingestion fix 2

--- project_1/data-ingestion/ride.py
+++ project_1/data-ingestion/ride.py
@@ -1,25 +1,14 @@
-import json
+from msgspec.structs import asdict
+from msgspec import Struct
+from msgspec.json import encode
 
-class Ride:
-    def __init__(self, ride_id, rideable_type, 
-        started_at, ended_at, start_station_id, end_station_id):
+class Ride(Struct):
+    ride_id: str
+    rideable_type: str
+    started_at: str
+    ended_at: str
+    start_station_id: str
+    end_station_id: str
 
-        self.ride_id = ride_id 
-        self.rideable_type = rideable_type
-        self.started_at = started_at
-        self.ended_at = ended_at
-        self.start_station_id = start_station_id
-        self.end_station_id = end_station_id
-
-    def to_dict(self):
-        return {
-            "ride_id": self.ride_id,
-            "rideable_type": self.rideable_type,
-            "started_at": self.started_at,
-            "ended_at": self.ended_at,
-            "start_station_id": self.start_station_id,
-            "end_station_id": self.end_station_id,
-        }
-
-    def to_json(self):
-        return json.dumps(self.to_dict())
+    def to_msg(self):
+        return encode(self)
